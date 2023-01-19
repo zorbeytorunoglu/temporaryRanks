460325fdfd667a71148022b47b58a141b28ed451
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-01-19 05:56:09 +0300

    loadData & saveData methods are added

--- src/main/kotlin/com/zorbeytorunoglu/temporaryRanks/TemporaryRanks.kt
+++ src/main/kotlin/com/zorbeytorunoglu/temporaryRanks/TemporaryRanks.kt
@@ -1,20 +1,23 @@
 package com.zorbeytorunoglu.temporaryRanks
 
 import com.zorbeytorunoglu.kLib.MCPlugin
+import com.zorbeytorunoglu.kLib.configuration.Resource
 import com.zorbeytorunoglu.kLib.configuration.createYamlResource
 import com.zorbeytorunoglu.kLib.task.Scopes
 import com.zorbeytorunoglu.kLib.task.suspendFunctionAsync
 import com.zorbeytorunoglu.temporaryRanks.commands.TemporaryRanksCMD
+import com.zorbeytorunoglu.temporaryRanks.listeners.ExpireOnJoin
 import com.zorbeytorunoglu.temporaryRanks.messages.MessageContainer
 import com.zorbeytorunoglu.temporaryRanks.rank.RankManager
 import kotlinx.coroutines.launch
-import org.bukkit.Bukkit
 import org.bukkit.scheduler.BukkitRunnable
 
 class TemporaryRanks: MCPlugin() {
 
     lateinit var messageContainer: MessageContainer
     lateinit var rankManager: RankManager
+
+    private lateinit var dataResource: Resource
 
     override fun onEnable() {
         super.onEnable()
@@ -24,10 +27,20 @@ class TemporaryRanks: MCPlugin() {
         rankManager = RankManager(this)
         rankManager.loadRanks(createYamlResource("ranks.yml").load())
 
+        dataResource = createYamlResource("data.yml").load()
+
+        rankManager.loadData(dataResource)
+
         TemporaryRanksCMD(this)
+        ExpireOnJoin(this)
 
         ExpirationChecker(this).runTaskTimerAsynchronously(this,20*20L,60*60*20L)
 
+    }
+
+    override fun onDisable() {
+        super.onDisable()
+        rankManager.saveData(rankManager.resetDataFile(dataResource))
     }
 
     private class ExpirationChecker(private val plugin: TemporaryRanks): BukkitRunnable() {
